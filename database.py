bdd6db8f25446b84913d524a7d35f5f7615e17e5
Me&MyDemons <bolsunros@mail.ru>
2024-02-18 14:41:46 +0700

    Added database.

---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class UserData(Base):
+    __tablenme__ = 'user_data'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    user_name = sa.Column(sa.Text, nullable=False)
+    password = sa.Column(sa.Text, nullable=False)
+
+
+class Albums(Base):
+    __tablename__ = 'albums'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('user_data.id'))
+    album_name = sa.Column(sa.Text)
+    images = sa.Column(sa.Text)
