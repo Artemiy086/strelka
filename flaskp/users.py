05f6be3065760907cf1c4ab1fe6f2fcce81c6f5e
TS1UL <artemy.tsiulin@gmail.com>
2024-02-21 17:54:38 +0300

    the minimum project template v0.001.02

--- flaskp/users.py
+++ flaskp/users.py
@@ -1,14 +1,15 @@
 from flask import current_app, render_template, Blueprint
 
+
 app = current_app
 bp_users = Blueprint('users', __name__, url_prefix='')
 
 
-@bp.route("/")
+@bp_users.route("/")
 def index():
     return render_template("index.html", cards_list=[])
 
 
-@bp.route("/card")
+@bp_users.route("/card")
 def test_card():
     return render_template("card.html")
